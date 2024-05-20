85180f1bff8b149a1d50a56c95c653f4f6ae17a6
Elomda Sultan Abdelaziz <omda732000@gmail.com>
2024-05-20 05:57:19 +0200

    task 1 code

---
+++ Task 1/task 1.js
@@ -0,0 +1,40 @@
+//   1
+
+// alert('Welcome to my site');
+// var ent_name = prompt('Name','');
+// document.write('Welcome, ' + ent_name);
+
+//  2
+
+// var x = 5;
+// console.log(x);
+// console.log(y);
+// var y = 7;
+// console.log('y = undefined needs to be initialized before it is used');
+
+//  3
+
+// function test(){
+//     for (let i = 0; i < 10; i++) {
+//         alert(i);
+//         alert (x);  // x = Cannot access x before initialization
+//         let x=10;
+//         }
+//     document.write(i); // i =  it's an let so can't used outdoor his scope // i is not defined
+// }
+
+// test();
+
+
+
+
+//    var
+
+// work on a global scope or functional scope        
+// can use until define ( return undefine )          
+// can Redeclare it              
+
+//     let
+// work on small scope  { }
+// can't call it until define ( return error )
+// can't Redeclare it
