ba05af8ec9edebd34a7ed82b9b8239b40c45b7ad
Elomda Sultan Abdelaziz <omda732000@gmail.com>
2024-05-24 21:51:20 +0200

    Another Answer For Form Validation

--- Task 4/Task 4(form).js
+++ Task 4/Task 4(form).js
@@ -2,14 +2,43 @@ function showTerms(){
     document.getElementById('terms-text').innerText='Long text of terms and conditions...';
 }
 
-function checkChar(event){
-    let key = event.key;
-    if(!isNaN(key) || !(key>='a' && key <='z' || key>='A' && key <='Z')){
-        event.preventDefault()
-    }
-}
-
 function submitData(){
     let newWindow=window.open('index.html');
     newWindow.alert('submit Successfully');
 }
+
+document.getElementById('f-btn').addEventListener('click',setValidate)
+
+function setValidate(){
+    document.forms[0].terms.required=true;
+    document.forms[0].name.required=true;
+    // document.getElementById('terms').required=true;
+    // document.getElementById('name').required=true;
+    let inText=document.getElementById('name').value;
+    for(i in inText){
+        if(!(inText[i]>='a' && inText[i] <='z' || inText[i]>='A' && inText[i] <='Z')){
+            alert('Only Alphapet');
+            document.getElementById('name').value ='';
+            break;
+        }
+    }
+    document.forms[0].addEventListener('submit',submitData);
+}
+
+
+    // Another Ans 
+// function showTerms(){
+//     document.getElementById('terms-text').innerText='Long text of terms and conditions...';
+// }
+
+// function checkChar(event){
+//     let key = event.key;
+//     if(!isNaN(key) || !(key>='a' && key <='z' || key>='A' && key <='Z')){
+//         event.preventDefault()
+//     }
+// }
+
+// function submitData(){
+//     let newWindow=window.open('index.html');
+//     newWindow.alert('submit Successfully');
+// }
